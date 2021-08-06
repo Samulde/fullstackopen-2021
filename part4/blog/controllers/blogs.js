799b8e577617db7ca74a94a94f2fc00a3eeca258
samulde <samulde@gmail.com>
2021-08-06 14:24:48 +1000

    Complete 4.8

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,5 +1,5 @@
 const blogsRouter = require('express').Router()
-const Blog = require('../models/person')
+const Blog = require('../models/blog')
 
 blogsRouter.get('/blogs', (request, response) => {
   Blog
