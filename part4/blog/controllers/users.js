041f4fa2583f6ec1461409953359983d598c57c6
samulde <samulde@gmail.com>
2021-08-10 22:25:21 +1000

    Complete 4.17

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -31,7 +31,7 @@ userRouter.get('/', async (request, response) => {
     
     const users = await User
         .find({})
-        .populate('blogs')
+        .populate('blogs', { url: 1, title: 1, author: 1})
     response.json(users)
 
 })
